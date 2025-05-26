89bdb2404247e9bcdd85d9bd69e61ec3fc46cf27
Micah Butt <micahbutt@outlook.com>
2025-05-26 14:14:44 +0100

    First

---
+++ src/client/java/com/chiefminingdad/autoplayer/AutoPlayerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.chiefminingdad.autoplayer;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutoPlayerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/chiefminingdad/autoplayer/ModItems.java
@@ -0,0 +1,36 @@
+package com.chiefminingdad.autoplayer;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems{
+	public static Item register(String name, Function<Item.Settings, Item> ItemFactory, Item.Settings settings) {
+		// Create the item key.
+		RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(AutoPlayer.MOD_ID, name));
+
+		// Create the item instance.
+		Item item = ItemFactory.apply(settings.registryKey(itemKey));
+
+		// Register the item.
+		Registry.register(Registries.ITEM,itemKey,item);
+
+		return item;
+	}
+
+	public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Settings());
+
+	public static void initialize() {
+		// Get the event for modifying entries in the ingredients group.
+		// And register an event handler that adds our suspicious item to the ingredients group.
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+				.register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+	}
+}
