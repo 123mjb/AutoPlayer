f5576ab78bfcd7f93755186be605de00d267d673
Micah Butt <micahbutt@outlook.com>
2025-05-26 14:15:38 +0100

    Secons

--- src/main/java/com/chiefminingdad/autoplayer/AutoPlayer.java
+++ src/main/java/com/chiefminingdad/autoplayer/AutoPlayer.java
@@ -2,18 +2,8 @@ package com.chiefminingdad.autoplayer;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.function.Function;
-
-
 
 public class AutoPlayer implements ModInitializer {
 	public static final String MOD_ID = "autoplayer";
