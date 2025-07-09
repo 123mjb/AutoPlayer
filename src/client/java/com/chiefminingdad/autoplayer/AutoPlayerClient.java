4481cffe2370c0fc033d0187545909dcc2c3b5c9
Micah Butt <micahbutt@outlook.com>
2025-07-09 14:15:50 +0100

    move forward for 2 secs!

--- src/client/java/com/chiefminingdad/autoplayer/AutoPlayerClient.java
+++ src/client/java/com/chiefminingdad/autoplayer/AutoPlayerClient.java
@@ -1,6 +1,13 @@
 package com.chiefminingdad.autoplayer;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AutoPlayerClient implements ClientModInitializer {
 	@Override
@@ -8,5 +15,28 @@ public class AutoPlayerClient implements ClientModInitializer {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		ScreenManager screenManager = new ScreenManager();
 		screenManager.init();
+
+		KeyBinding StartMovingKeyBind = KeyBindingBuilder.BuildKeyBind("StartMoving", true, GLFW.GLFW_KEY_J, "movecontroller");
+
+		AtomicBoolean MoveForwards = new AtomicBoolean(false);
+		AtomicInteger TimeLeft = new AtomicInteger();
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (StartMovingKeyBind.wasPressed()) {
+				if (!MoveForwards.get()){ MoveForwards.set(true);
+					TimeLeft.set(40);
+				}
+
+			}
+			if (MoveForwards.get()){
+				if(TimeLeft.get()==0){
+					MoveForwards.set(false);
+					MinecraftClient.getInstance().options.forwardKey.setPressed(false);
+				}
+				else{
+					MinecraftClient.getInstance().options.forwardKey.setPressed(true);
+					TimeLeft.set(TimeLeft.get()-1);
+				}
+			}
+		});
 	}
 }
--- src/client/java/com/chiefminingdad/autoplayer/mixin/client/DeclaringVariablesInTick.java
+++ src/client/java/com/chiefminingdad/autoplayer/mixin/client/DeclaringVariablesInTick.java
@@ -1,13 +1,14 @@
 package com.chiefminingdad.autoplayer.mixin.client;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
+@Mixin(ClientTickEvents.class)
+public class DeclaringVariablesInTick {
 	@Inject(at = @At("HEAD"), method = "run")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftClient.run()V
