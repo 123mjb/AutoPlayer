8f4e196f312605f11745c630f8a9b50fb14e4ed4
Micah Butt <micahbutt@outlook.com>
2025-07-11 08:56:48 +0100

    Making Simpler More Complicated

--- src/client/java/com/chiefminingdad/autoplayer/MoveUntil.java
+++ src/client/java/com/chiefminingdad/autoplayer/MoveUntil.java
@@ -3,37 +3,51 @@ package com.chiefminingdad.autoplayer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.KeyBinding;
 
 public class MoveUntil {
+    public MinecraftClient Instance;
     public ClientPlayerEntity player;
+    public KeyBinding Jump;
+    public KeyBinding Forwards;
     public boolean Move = false;
-    public float desiredrotation;
-	public int desiredx;
-	public int desiredy;
-	public int desiredz;
+    public float desiredRotation;
+	public int desiredX;
+	public int desiredY;
+	public int desiredZ;
 
     MoveUntil(){
-        if (MinecraftClient.getInstance()!=null) player = MinecraftClient.getInstance().player;
+        if (MinecraftClient.getInstance()!=null) {
+            Instance = MinecraftClient.getInstance();
+            player = Instance.player;
+            Jump =Instance.options.jumpKey;
+            Forwards = Instance.options.forwardKey;
+        }
     }
 
     public void init(MoveUntil moveUntil){
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            moveUntil.MoveCorrectDirection();
-        });
+        ClientTickEvents.END_CLIENT_TICK.register(client -> moveUntil.MoveCorrectDirection());
     }
 
     public void SetVars(int x,int y,int z, float rot){
-        this.desiredrotation = rot;
-        this.desiredx = x;
-        this.desiredy = y;
-        this.desiredz = z;
+        this.desiredRotation = rot;
+        this.desiredX = x;
+        this.desiredY = y;
+        this.desiredZ = z;
 
     }
 
     public void MoveCorrectDirection(){
         if (this.Move) {
-            player.setYaw(this.desiredrotation);
+            player.setYaw(this.desiredRotation);
+
+            if (desiredX != player.getX() | desiredY != player.getY() | desiredZ != player.getZ()) {
+                Jump.setPressed(desiredY > player.getY());
+                Forwards.setPressed(desiredX != player.getX() | desiredZ != player.getZ());
+            }
+            else this.Move = false;
         }
+
     }
 
 }
--- src/client/java/com/chiefminingdad/autoplayer/mixin/client/DeclaringVariablesInTick.java
+++ src/client/java/com/chiefminingdad/autoplayer/mixin/client/DeclaringVariablesInTick.java
@@ -11,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientTickEvents.class)
 public class DeclaringVariablesInTick {
-	@Inject(at = @At("CTOR_HEAD"), method = "<init>")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of CientTickEvents.<init>()
-	}
+//	@Inject(at = @At("CTOR_HEAD"), method = "<init>")
+//	private void init(CallbackInfo info) {
+//		// This code is injected into the start of CientTickEvents.<init>()
+//	}
 
 
 }
