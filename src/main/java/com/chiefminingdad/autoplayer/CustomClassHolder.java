6a6ac00c0e35dba3a0c1b2d2082f2e83afb9c7f1
Micah Butt <micahbutt@outlook.com>
2025-09-17 17:15:35 +0100

    Argument Works, now work on writeup!!! Micah!!!!

--- src/main/java/com/chiefminingdad/autoplayer/CustomClassHolder.java
+++ src/main/java/com/chiefminingdad/autoplayer/CustomClassHolder.java
@@ -14,16 +14,9 @@ public class CustomClassHolder {
             String string = reader.readString();
             String[] split = string.split(",",3);
 
-//            int x = Integer.parseInt(split[0].trim());
-//            int y = Integer.parseInt(split[1].trim());
-//            int z = Integer.parseInt(split[2].trim());
             try{x = Integer.parseInt(split[0]);} catch(Exception e) {x = Integer.MAX_VALUE;}
             try{y = Integer.parseInt(split[1]);} catch(Exception e) {y = Integer.MAX_VALUE;}
             try{z = Integer.parseInt(split[2]);} catch(Exception e) {z = Integer.MAX_VALUE;}
-
-//            x = (!split[0].trim().isEmpty())?Integer.parseInt(split[0]):null;
-//            y = (!split[1].trim().isEmpty())?Integer.parseInt(split[1]):null;
-//            z = (!split[2].trim().isEmpty())?Integer.parseInt(split[2]):null;
 
             return new Integer[] {x,y,z};
 
