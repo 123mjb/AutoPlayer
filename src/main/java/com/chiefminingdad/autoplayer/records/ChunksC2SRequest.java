625f49d53c0a2b758711291520913a2bd78073c8
Micah Butt <micahbutt@outlook.com>
2025-09-26 09:58:07 +0100

    No uppercase allowed

--- src/main/java/com/chiefminingdad/autoplayer/records/ChunksC2SRequest.java
+++ src/main/java/com/chiefminingdad/autoplayer/records/ChunksC2SRequest.java
@@ -7,7 +7,7 @@ import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.util.Identifier;
 
 public record ChunksC2SRequest(Integer x, Integer z) implements CustomPayload {
-    public static final Identifier CHUNK_REQUEST_ID = Identifier.of("autoplayer:ChunkRequester");
+    public static final Identifier CHUNK_REQUEST_ID = Identifier.of("autoplayer:chunkrequester");
     public static final CustomPayload.Id<ChunksC2SRequest> ID = new CustomPayload.Id<>(CHUNK_REQUEST_ID);
     public static final PacketCodec<RegistryByteBuf,ChunksC2SRequest> CODEC = PacketCodec.tuple(PacketCodecs.INTEGER,ChunksC2SRequest::x,PacketCodecs.INTEGER,ChunksC2SRequest::z,ChunksC2SRequest::new);
 
